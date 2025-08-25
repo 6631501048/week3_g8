27b0eb87d2c563d97df0cd801a72c6ceca318267
6631504049-stack <6631501049@lamduan.mfu.ac.th>
2025-08-25 15:38:48 +0700

    complete 1+2

--- bin/week3_g8.dart
+++ bin/week3_g8.dart
@@ -1,4 +1,4 @@
-import 'package:week3_g8/week3_g8.dart' as week3_g8;
+import 'package:week3_g8/week3_g8.dart';
 import 'package:http/http.dart' as http;
 import 'dart:io';
 
@@ -6,9 +6,144 @@ import 'dart:convert';
 //============================================================
 
 //================= Fea 1+2 =================
+void main() async {
+  int key = 1;
+  while (key == 1) {
+    print("--Account--");
+    print("1. Login");
+    print("2. Registration");
+    stdout.write("Choose the number: ");
+    String? service = stdin.readLineSync();
+    if (service == '1') {
+      await login();
+      key = 0;
+    } else if (service == '2') {
+      await register();
+    } else {
+      print("please select the number");
+    }
+  }
+  print("---------- Bye ----------");
+}
 
+Future<void> register() async {
+  print("==== Registration ====");
+  stdout.write("Username: ");
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write("Password: ");
+  String? password = stdin.readLineSync()?.trim();
+  if (username == null || password == null) {
+    print("Incomplete input");
+    return;
+  }
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('http://localhost:3000/register');
+  final response = await http.post(url, body: body);
+  if (response.statusCode == 200) {
+    final result = response.body;
+    print(result);
+  } else if (response.statusCode == 401 || response.statusCode == 500) {
+    final result = response.body;
+    print(result);
+  } else {
+    print("Unknown error");
+  }
+}
+
+Future<void> login() async {
+  print("===== Login =====");
+  // Get username and password
+  stdout.write("Username: ");
+  String? username = stdin.readLineSync()?.trim();
+  stdout.write("Password: ");
+  String? password = stdin.readLineSync()?.trim();
+  if (username == null || password == null) {
+    print("Incomplete input");
+    return;
+  }
+  final body = {"username": username, "password": password};
+  final url = Uri.parse('http://localhost:3000/login');
+  final response = await http.post(url, body: body);
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+    final userId = data['userId'];
+    print("Login OK!");
+    await showmenu(userId);
+  } else if (response.statusCode == 401 || response.statusCode == 500) {
+    final result = response.body;
+    print(result);
+  } else {
+    print("Unknown error");
+  }
+}
+
+Future<void> showmenu(int userId) async {
+  String? choice;
+  do {
+    print("======= Expense tracking app =======");
+    print("1. Show all");
+    print("2. Today's expense");
+    print("3. Exit");
+    stdout.write("Choose... ");
+    choice = stdin.readLineSync();
+    if (choice == "1") {
+      await showAllExpenses(userId);
+    } else if (choice == "2") {
+      await showTodayExpenses(userId);
+    } else if (choice != "3") {
+      print("Invalid choice");
+    }
+  } while (choice != "3");
+}
+
+Future<void> showAllExpenses(int userId) async {
+  final url = Uri.parse('http://localhost:3000/expenses/$userId');
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    final List expenses = jsonDecode(response.body);
+    if (expenses.isEmpty) {
+      print("Notthing bruhh");
+    } else {
+      int total = 0;
+      print("------------ All Expenses ------------");
+      for (var exp in expenses) {
+        print(
+          " ${exp['id']}. ${exp['items']} : ${exp['paid']}฿ :${exp['date']}",
+        );
+        total += (exp['paid'] as num).toInt();
+      }
+      print("Total expense: ${total}฿ ");
+    }
+  } else {
+    print("Error fetching expenses: ${response.body}");
+  }
+}
+
+Future<void> showTodayExpenses(int userId) async {
+  final url = Uri.parse('http://localhost:3000/expenses/today/$userId');
+  final response = await http.get(url);
+  if (response.statusCode == 200) {
+    final List expenses = jsonDecode(response.body);
+    if (expenses.isEmpty) {
+      print("Notthing for today");
+    } else {
+      print("------------ Today's Expenses ------------");
+      int total = 0;
+      for (var exp in expenses) {
+        print(
+          " ${exp['id']}. ${exp['items']} : ${exp['paid']}฿ :${exp['date']}",
+        );
+        total += (exp['paid'] as num).toInt();
+      }
+      print("Total expenses: ${total}฿ ");
+    }
+  } else {
+    print("Error fetching today's expenses: ${response.body}");
+  }
+}
 
 //================= Fea 3 =================
+
 
 //================= Fea 4 =================
 
